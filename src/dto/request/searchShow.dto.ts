88cc8323cd71462906e1d9759adf15938913b52a
Bhavya <bhavyampatel25@gmail.com>
2023-03-20 10:04:27 +0530

    pagination

--- src/dto/request/searchShow.dto.ts
+++ src/dto/request/searchShow.dto.ts
@@ -1,14 +1,19 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 export class SearchShowDTO {
-  
   @IsOptional()
   @IsString()
   @ApiProperty({
-    name:'movieName',
-    description:"name of movie to search",
-    required:true,
-    type:'string'
+    name: "movieName",
+    description: "name of movie to search",
+    required: true,
+    type: "string",
   })
-  movieName: string
+  movieName: string;
+
+  @IsOptional()
+  @Transform((o) => parseInt(o.value))
+  @IsNumber()
+  page: number;
 }
